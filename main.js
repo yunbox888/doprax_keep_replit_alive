737a820a0fbadeb3f1ff275a0f853a3884dce245
yunbox888 <116336639+yunbox888@users.noreply.github.com>
2022-12-27 21:49:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http')
 var request = require('request');
 
 // 这里填你的replit应用的url，多个url用逗号隔开
-let replit_app_urls = ["https://vv.mrzyang.repl.co", "https://replit-trojan.yangzhang23.repl.co"]
+let replit_app_urls = ["https://yunbox1.yunbox888.repl.co", "https://yunbox2.kerryliuchengyi.repl.co"]
 
 var port = 4000
 var server = http.createServer(function (request, response) {
